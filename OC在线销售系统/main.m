0a21065fbabc0a42154654c69eaaa9ebda5f142d
Larry <Larry@Larry-MacBook-Pro.local>
2015-08-19 20:59:30 +0800

    Initial Commit

---
+++ OC在线销售系统/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC在线销售系统
+//
+//  Created by Larry on 15/8/19.
+//  Copyright (c) 2015年 Larry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
